ee5d50f7299405e81cc1e74b353a05f00005331c
Monica Hong <momo.star@gmail.com>
2025-02-09 20:59:17 -0800

    wtf toggle why

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,25 @@
 // src/components/Home.jsx
 import React from "react";
+import "./ChatInterface.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="chat-interface">
       <h1>Welcome to SoLogue</h1>
       <p>Have an insightful conversation with yourself!</p>
+      <p>Type to get started...</p>
+      <div class="toggle-button">
+        <div class="btn btn-rect" id="button-13">
+          <input type="checkbox" class="checkbox" />
+          <div class="knob">
+            <span></span>
+          </div>
+          <div class="btn-bg"></div>
+        </div>
+      </div>
+      <label>
+        <input type="text" className="text-field" placeholder="Enter text" />
+      </label>
     </div>
   );
 };
