3d465abb5c8c40cc54a79c576cc25c80fd32f3c6
Monica Hong <momo.star@gmail.com>
2025-02-10 14:31:11 -0800

    instantiate firebase auth

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG4xty_4mkjh11Yzx-Q3sxbtSQ1Do0pDE",
+  authDomain: "sologue-app.firebaseapp.com",
+  projectId: "sologue-app",
+  storageBucket: "sologue-app.firebasestorage.app",
+  messagingSenderId: "46491937232",
+  appId: "1:46491937232:web:50e6361ae0287f66d1f1d5",
+  measurementId: "G-LX25WX8028",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
