c34368fac0b76b2e41ef051591c2dc1d3e426b79
Monica Hong <momo.star@gmail.com>
2025-02-11 20:11:44 -0800

    janky signup

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,18 +1,21 @@
 // src/components/Signup.js
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import { auth } from "../firebaseConfig";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  let navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       alert("User signed up successfully!");
+      navigate("/login");
     } catch (error) {
       alert(error.message);
     }
