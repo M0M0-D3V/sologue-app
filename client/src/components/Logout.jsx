16b629e48e0c81e89f3096a85cb0ebbfd3ec0e02
Monica Hong <46170795+M0M0-D3V@users.noreply.github.com>
2025-02-11 22:56:08 -0800

    Develop (#2)
    * initial commit
    
    * add basic chat box
    
    * style chat interface
    
    * add routes
    
    * wtf toggle why
    
    * implement sidebarmenu slide out
    
    * freaking closes now yay
    
    * menu on top
    
    * refactor menu
    
    * add App class to chat interface for styling consistency
    
    * cleanup, remove label
    
    * instantiate firebase auth
    
    * firebase auth setup
    
    * implement user navigation on auth state change and update firebase config for Google authentication
    
    * implement user authentication state handling in App and Home components
    
    * login auth working
    
    * janky signup
    
    * connect firebase db
    
    * logout broken...
    
    * fixed logout

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,11 +1,20 @@
 import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../firebaseConfig";
 
-export default signOut(auth)
-  .then(() => {
-    // Sign-out successful - navigate to /
-  })
-  .catch((error) => {
-    // An error happened.
-    console.error(error);
-  });
+const Logout = () => {
+  const navigate = useNavigate();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      navigate("/");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.error(error);
+    });
+
+  return null;
+};
+
+export default Logout;
